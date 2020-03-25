9ac90b8a70719205b9bda10f8a28e6bd1558a4d4
Ravi-Raushan <krravi015@gmail.com>
2020-03-26 00:45:58 +0530

    [Ravi raushan]Added. Mood Analyser Factory class which use reflator to create class object and constructor

---
+++ reflationConcept/MoodAnalyserFactory.java
@@ -0,0 +1,43 @@
+package com.reflector.mood;
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+
+public class MoodAnalyserFactory {
+    public static MoodAnalyser createMoodAnalyserObject(String msg) throws MoodAnalysesException {
+        try {
+            Class<?> moodAnalyserClass = Class.forName("com.reflector.mood.MoodAnalyser");
+            Constructor<?> moodConstructor = moodAnalyserClass.getConstructor(String.class);
+            return (MoodAnalyser) moodConstructor.newInstance(msg);
+        } catch (ClassNotFoundException e) {
+            throw new MoodAnalysesException(MoodAnalysesException.exceptionType.NO_SUCH_CLASS,"NO_SUCH_CLASS_ERROR");
+        } catch (InstantiationException e) {
+            throw new MoodAnalysesException(MoodAnalysesException.exceptionType.OBJECT_CREATION_ISSUE,"OBJECT_CREATION_ISSUE");
+        } catch (InvocationTargetException e) {
+            throw new MoodAnalysesException(MoodAnalysesException.exceptionType.METHOD_INVOCATION_ISSUE,"METHOD_INVOCATION_ISSUE");
+        } catch (NoSuchMethodException e) {
+            throw new MoodAnalysesException(MoodAnalysesException.exceptionType.NO_SUCH_METHOD,"NO_SUCH_METHOD_ERROR");
+        } catch (IllegalAccessException e) {
+            throw new MoodAnalysesException(MoodAnalysesException.exceptionType.NO_ACCESS,"NO_ACCESS");
+        }
+    }
+    public static MoodAnalyser createMoodAnalyserObject() throws MoodAnalysesException {
+        try {
+            Class<?> moodAnalyserClass = Class.forName("com.reflector.mood.MoodAnalyser");
+            Constructor<?> moodConstructor = moodAnalyserClass.getConstructor(Integer.class);
+            return (MoodAnalyser) moodConstructor.newInstance();
+
+        }catch (ClassNotFoundException e) {
+            throw new MoodAnalysesException(MoodAnalysesException.exceptionType.NO_SUCH_CLASS,"NO_SUCH_CLASS_ERROR");
+        } catch (InstantiationException e) {
+            throw new MoodAnalysesException(MoodAnalysesException.exceptionType.OBJECT_CREATION_ISSUE,"OBJECT_CREATION_ISSUE");
+        } catch (InvocationTargetException e) {
+            throw new MoodAnalysesException(MoodAnalysesException.exceptionType.METHOD_INVOCATION_ISSUE,"METHOD_INVOCATION_ISSUE");
+        } catch (NoSuchMethodException e) {
+            throw new MoodAnalysesException(MoodAnalysesException.exceptionType.NO_SUCH_METHOD,"NO_SUCH_METHOD_ERROR");
+        } catch (IllegalAccessException e) {
+            throw new MoodAnalysesException(MoodAnalysesException.exceptionType.NO_ACCESS,"NO_ACCESS");
+        }
+    }
+}
+
